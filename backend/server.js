2282e49dc7c75aa298d301e96577b2dcc87ee826
PRAGATI AGNIHOTRI <69899356+pragati-agnihotri@users.noreply.github.com>
2025-05-08 17:51:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,34 @@ const doctorRoutes = require('./routes/doctorRoutes');
 require('dotenv').config();
 
 const app = express();
+const corsOptions = {
+  origin :' https://andaz-project.vercel.app/',
+  optionsSuccessStatus: 200,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // If you need to allow cookies, set this to true
+  allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Allow-Origin'], // Ensure these headers are allowed
+};
+
+app.use(cors(corsOptions)); // Apply CORS middleware
 app.use(cors());
 app.use(express.json());
-
+// Root route
+app.get('/', (req, res) => {
+  res.status(200).send('Hey this is my API running 🥳');
+});
 app.use('/api/doctors', doctorRoutes);
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    error: {
+     message: "500: INTERNAL_SERVER_ERROR",
+      code: "FUNCTION_INVOCATION_FAILED",
+      id: "bom1::6d4v8-1725912118020-2f3bc51fbb3a",
+      suggestion: "If you are a visitor, contact the website owner or try again later. If you are the owner, learn how to fix the error and check the logs."
+    }
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
